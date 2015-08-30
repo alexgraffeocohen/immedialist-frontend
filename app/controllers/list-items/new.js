8a2998b414556e85a16fa2193eeac888241629ec
Alex Wilkinson <alex.wilkinson@me.com>
2015-08-30 16:46:51 -0400

    Refactor: set model property on listItemsNewController

--- app/controllers/list-items/new.js
+++ app/controllers/list-items/new.js
@@ -3,25 +3,26 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     save() {
-      var formParams = {name: this.get('name'), mediaType: this.get('mediaType')};
-      var requestedItem = this.store.createRecord(
-        'requested-item', {name: formParams.name, mediaName: formParams.mediaType}
-      );
-      requestedItem.save().then((item) => {
+      this.setMediaNameIfNull()
+      this.get('model').save().then((item) => {
         var listItem = this.store.createRecord(
-          'listItem', {name: formParams.name, item: item}
+          'listItem', {name: this.get('model.name'), item: item}
         );
         listItem.save();
         this.clearNameFromForm();
       });
     },
-
-    selectMediaType(selection) {
-      this.set('mediaType', selection);
+  },
+  setMediaNameIfNull() {
+    // TODO: why does emberx-select not set whatever the default value of the
+    // select is on initial load?
+    if(this.get('model.mediaName') === undefined) {
+      // TODO: try to have this not break if the first option in the select
+      // changes independently
+      this.set('model.mediaName', 'movie');
     }
   },
   clearNameFromForm() {
     this.set('name', '');
   },
-  mediaType: 'movie' // TODO: have this always be in sync with value of select
 });
--- app/routes/list-items.js
+++ app/routes/list-items.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   renderTemplate() {
+    var listItemsNewController = this.controllerFor('listItems/new');
+    // A little counter-intuitive, but we need this to be a requestedItem
+    // rather than a listItem because we need to create a requestedItem before
+    // creating a listItem. This slight confusion is worth it because setting
+    // the model allows us some nice refactoring and clarity in other places.
+    listItemsNewController.set('model', this.store.createRecord('requestedItem'));
+
     this.render('list-items');
     this.render('list-items/new', {
       into: 'list-items',
       outlet: 'new',
-      controller: this.controllerFor('listItems/new')
+      controller: listItemsNewController
     });
   },
 });
