d3be488649abc32f67ec2a990c740e683a0650f1
Alex Wilkinson <alex.wilkinson@me.com>
2015-08-31 13:38:15 -0400

    Add basic list-item show view

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('listItems', { path: '/' }, function(){
+    this.route('listItem', { path: ':listitem_id' });
   });
 });
 
