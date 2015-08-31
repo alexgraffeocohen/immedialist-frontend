8fcf3c93a43b784cbe488e32f1c951e86062b2c0
Alex Wilkinson <alex.wilkinson@me.com>
2015-08-30 20:43:22 -0400

    List-Item: port functionality from first draft of app

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteListItem: function() {
+      var listItem = this.get('listItem');
+      listItem.deleteRecord();
+      listItem.save();
+    }
+  },
+
+  mediaType: function() {
+    return this.get('listItem.mediaType').toLowerCase();
+  }.property('listItem'),
+  itemName: function() {
+    return this.get('listItem.name').dasherize();
+  }.property('listItem'),
+  itemDOMID: function() {
+    return "#" + this.get('itemName');
+  }.property('listItem'),
+  typeIcon: function() {
+    switch(this.get('listItem.mediaType')) {
+      case "movie":
+        return "fa-film";
+      case "show":
+        return "fa-video-camera";
+      case "actor":
+        return "fa-user";
+      case "director":
+        return "fa-user";
+      case "author":
+        return "fa-user";
+      case "artist":
+        return "fa-user";
+      case "song":
+        return "fa-music";
+      case "album":
+        return "fa-music";
+      case "book":
+        return "fa-book";
+    }
+  }.property('listItem')
 });
