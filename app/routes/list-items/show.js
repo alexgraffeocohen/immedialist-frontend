0380d99c7f0e32aef6813274d6682904a55911b4
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-01 17:31:02 -0400

    Use listItems/show router to choose whether to render item or search results

--- app/routes/list-items/show.js
+++ app/routes/list-items/show.js
@@ -4,5 +4,21 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     controller.set('model', model);
     controller.set('componentName', `${model.get('mediaType')}-detail`);
+  },
+
+  renderTemplate(controller, model) {
+    this.render('list-items/show');
+
+    if(model.get('resolved')) {
+      this.render('list-items/show/item', {
+        into: 'list-items/show',
+        outlet: 'show'
+      });
+    } else {
+      this.render('list-items/show/search', {
+        into: 'list-items/show',
+        outlet: 'show'
+      });
+    }
   }
 });
