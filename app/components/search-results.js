a0375cda7f48f92e943dde6f872deae3204ca3f3
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-01 22:20:40 -0400

    search-results: update style and layout of component

--- app/components/search-results.js
+++ app/components/search-results.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['col-md-offset-1', 'col-md-10'],
   searchResults: function() {
     return this.get('search.items');
+  }.property('search'),
+  mediaType: function() {
+    return this.get('search.mediaType').capitalize();
   }.property('search')
 });
