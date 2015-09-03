22d429e388b0c54e0b5451bb2bc496b8980b6481
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-02 22:09:51 -0400

    Only style based on the creator type and eliminate mention of the rest

--- app/services/icon-name-generator.js
+++ app/services/icon-name-generator.js
@@ -3,20 +3,11 @@ import Ember from 'ember';
 export default Ember.Object.extend({
   call() {
     switch(this.get('listItem.mediaType')) {
-      // TODO: remove other "person" cases besides "creator"
       case "movie":
         return "fa-film";
       case "show":
         return "fa-video-camera";
-      case "actor":
-        return "fa-user";
-      case "director":
-        return "fa-user";
-      case "author":
-        return "fa-user";
       case "creator":
-        return "fa-user";
-      case "artist":
         return "fa-user";
       case "song":
         return "fa-music";
