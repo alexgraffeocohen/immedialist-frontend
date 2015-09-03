f1937746afaf63cf513fe4bfacfc86d4ff16e9ea
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-03 15:29:35 -0400

    Media-Card Component: use bootstrap tooltips to display full title

--- app/components/search-results.js
+++ app/components/search-results.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['col-md-offset-1', 'col-md-10'],
+  didInsertElement: function() {
+    $('[data-toggle="tooltip"]').tooltip();
+  },
   searchResults: function() {
     return this.get('search.items');
   }.property('search'),
