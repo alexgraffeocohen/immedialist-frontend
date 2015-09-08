8affa5fe429d650b81ccb41472f83be6d2ee3458
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-08 16:34:31 -0400

    Media-Option: collapse media-card component into it to remove unneeded encapsulation

--- app/components/media-option.js
+++ app/components/media-option.js
@@ -2,8 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    selectItem(item) {
-      console.log(`in the media option component with ${item}`);
+    selectItem() {
+      console.log(`in the media option component with ${this.get('option')}`);
     }
-  }
+  },
+  charCountForTitle: 35,
+  cardTitleTooLong: function() {
+    let nameLength = this.get('option.name').length
+    let tooLong = nameLength > this.get('charCountForTitle');
+    return tooLong
+  }.property('option.name', 'charCountForTitle')
 });
