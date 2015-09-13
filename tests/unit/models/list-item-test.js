672f10147a21ea09f307e9302cf7eaa16bf1420a
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-13 17:58:58 -0400

    list-item-test: switch module for moduleForModel to pass JSHint
    FactoryGuy docs say you can use module instead of moduleForModel, but
    that causes JSHint issues (even though the test itself passes). Perhaps
    this will resolve itself later on.

--- tests/unit/models/list-item-test.js
+++ tests/unit/models/list-item-test.js
@@ -1,11 +1,15 @@
+import { moduleForModel, test } from 'ember-qunit';
 import Ember from 'ember';
 import FactoryGuy from 'ember-data-factory-guy';
 import startApp from '../../helpers/start-app';
-import { test } from 'ember-qunit';
 
 let App;
 
-module('ListItem', {
+// FactoryGuy docs say you can use module instead of moduleForModel, but that
+// causes JSHint issues (even though the test itself passes). Perhaps this will
+// resolve itself later on.
+
+moduleForModel('list-item', 'Unit | Model | list item', {
   beforeEach: function() {
     App = startApp();
   },
@@ -17,5 +21,6 @@ module('ListItem', {
 test('it calculates mediaType from its item', function(assert) {
   var item = FactoryGuy.make('item', {mediaType: 'movie'});
   var listItem = FactoryGuy.make('list-item', {item: item});
+
   assert.equal(listItem.get('mediaType'), 'movie');
 });
