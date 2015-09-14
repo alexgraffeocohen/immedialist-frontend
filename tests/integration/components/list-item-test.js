f392dbd75ff749c221d9b6bda2558d94498ee4e7
Alex Wilkinson <alex.wilkinson@me.com>
2015-09-14 00:19:49 -0400

    list-item component test: use factories to check template rendering

--- tests/integration/components/list-item-test.js
+++ tests/integration/components/list-item-test.js
@@ -1,26 +1,38 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
+import FactoryGuy from 'ember-data-factory-guy';
+import startApp from '../../helpers/start-app';
 import hbs from 'htmlbars-inline-precompile';
 
+let App;
+
 moduleForComponent('list-item', 'Integration | Component | list item', {
-  integration: true
+  integration: true,
+  setup: function () {
+    Ember.run(function () {
+      App = startApp();
+    });
+  },
+  teardown: function () {
+    Ember.run(function () {
+      App.destroy();
+    });
+  }
 });
 
-test('it renders', function(assert) {
+test('it renders content based on its item', function(assert) {
   assert.expect(2);
+
+  var item = FactoryGuy.make('item', {mediaType: 'movie'});
+  var listItem = FactoryGuy.make('list-item', {item: item});
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('listItem', listItem);
 
-  this.render(hbs`{{list-item}}`);
+  this.render(hbs`{{list-item listItem=listItem}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#list-item}}
-      template block text
-    {{/list-item}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.list-items__item-name').text().trim(),
+               `${listItem.get('name')}`);
+  assert.ok(this.$('i').hasClass('fa-film'));
 });
