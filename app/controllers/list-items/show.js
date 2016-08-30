04a276dface6a166411350aecd54e6a570c498e4
Alex Wilkinson <alex.wilkinson@me.com>
2016-08-29 23:51:40 -0400

    Bug Fix: transition to index before setting selected item
    If I didn't transition to the index, then the listItem.item associated
    was set, blanking out the requestedType attribute because the
    listItem.item is not a requestedItem anymore. This caused an error in
    the component helper, preventing any further transitions.

--- app/controllers/list-items/show.js
+++ app/controllers/list-items/show.js
@@ -4,12 +4,13 @@ export default Ember.Controller.extend({
   actions: {
     selectItem(item) {
       var model = this.get('model');
+      this.transitionToRoute('listItems');
+
       model.set('item', item);
       model.save().then(() => {
         this.
           get('flashMessages').
           success(`${item.get('name')} successfully selected!`);
-        this.transitionToRoute('listItems');
       });
     }
   },
